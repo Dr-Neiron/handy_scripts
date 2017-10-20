496f28cbf14336b11df07a09110996caaa40864f
Maxim Suldin <Maxim.Suldin@devel.iress.com.au>
2017-10-20 14:38:22 +1100

    add dup search function

--- dedup.py
+++ dedup.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import md5
 
 DEFAULT_LIMIT = 5 * 1024 * 1024
@@ -14,7 +15,34 @@ def md5sum(file_name, limit=None):
     return hash_md5.hexdigest()
 
 
+class CollisionError(Exception):
+    pass
+
+
+def dedup(dir):
+    hash_dict = {}
+    duplications = {}
+    for subdir, _, files in os.walk(dir):
+        for file in files:
+            full_name = os.path.join(subdir, file)
+            short_md5 = md5sum(full_name, limit=DEFAULT_LIMIT)
+            if short_md5 in hash_dict:
+                original_full_md5 = md5sum(hash_dict[short_md5], limit=None)
+                full_md5 = md5sum(full_name)
+                if original_full_md5 != full_md5:
+                    raise CollisionError('{} and {} has same short md5'
+                                         .format(hash_dict[full_md5], full_name))
+                if short_md5 in duplications:
+                    duplications[short_md5].append(full_name)
+                else:
+                    duplications[short_md5] = [hash_dict[short_md5], full_name]
+            else:
+                hash_dict[short_md5] = full_name
+
+    for _, value in duplications.items():
+        print(value)
+        print('\n')
+
+
 if __name__ == '__main__':
-    print(md5sum(
-        'D:\\ms_work\\items\\IOSPLUS-17933 Dynamic Limit unit tests are randomly failing due to timeout on the script execution\\Release_x64\\1log',
-        limit=DEFAULT_LIMIT))
+    dedup('D:\\ms_tmp\\rc_retail')
